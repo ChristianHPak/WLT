eee1c9eaa684606c6560fa4dae1ae12288700ff7
Christian Pak <iamchristianpak@gmail.com>
2019-09-18 17:41:04 -0600

    added mongo, and started on the edit page

---
+++ wishlist/src/components/pages/edit.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Button, Jumbotron, Container } from 'reactstrap';
+
+class App extends Component {
+    state = {
+      title: '',
+      available: false,
+      price: 0,
+      description: '',
+      provider: '',
+      picture: ''
+    }
+  
+    updateTitle = title => {
+    }
+    updateAvailability = available => {
+    }
+    updatePrice = price => {
+    }
+    updateDescription = description => {
+    }
+    updateProvider = provider => {
+    }
+    updatePicture = picture => {
+    }
+
+
+    render() {
+        return (
+            
+        )
+    }
+  }
--- wishlist/src/components/pages/index.js
+++ wishlist/src/components/pages/index.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Jumbotron, Container } from 'reactstrap';
 
-function Index(props) {
-    return (
-        <>
-            <header>
-                <div className="header">Wish List Therapy</div>
-            </header>
-            <div class="container">
-                <div class="row text-center">
-                    <div class="col-4">
-                        <button type="button" className="btn btn-default">Left</button>
-                    </div>
-                    <div class="col-4">
-                        <button type="button" className="btn btn-default">Middle</button>
-                    </div>
-                    <div class="col-4">
-                        <button type="button" className="btn btn-default">Right</button>
+class Index extends Component {
+    render() {
+        return (
+            <>
+                <header>
+                    <div className="header">Wish List Therapy</div>
+                </header>
+                <div className="container">
+                    <div className="row text-center">
+                        <div className="col-4">
+                            <button type="button" className="btn btn-default">Left</button>
+                        </div>
+                        <div className="col-4">
+                            <button type="button" className="btn btn-default">Middle</button>
+                        </div>
+                        <div className="col-4">
+                            <button type="button" className="btn btn-default">Right</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    );
+            </>
+        );
+    }
 };
 
 export default Index;
